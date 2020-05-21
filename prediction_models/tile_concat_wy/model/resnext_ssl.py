1aafde67ed3c28bf31b0e60febb3f3b1f3cd74ff
wenyuan <liwenyuan.zju@gmail.com>
2020-05-20 21:10:22 -0700

    commit regression on medium level resolution

--- prediction_models/tile_concat_wy/model/resnext_ssl.py
+++ prediction_models/tile_concat_wy/model/resnext_ssl.py
@@ -29,8 +29,6 @@ class Model(nn.Module):
         x_out: [bs, N]
         """
         bs, n, c, h, w = x.shape
-        print(x.shape)
-        exit()
         x = x.view(-1, c, h, w)  # x: bs*N x 3 x 128 x 128
         x = self.enc(x)  # x: bs*N x C x 4 x 4
         _, c, h, w = x.shape
