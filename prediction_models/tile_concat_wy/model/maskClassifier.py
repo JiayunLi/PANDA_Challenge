6e320477de00292cd66466ebee42348b4fb0cdf2
wenyuan <liwenyuan.zju@gmail.com>
2020-05-26 16:08:00 -0700

    commit mask classifier

--- prediction_models/tile_concat_wy/model/maskClassifier.py
+++ prediction_models/tile_concat_wy/model/maskClassifier.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-def extract_features(mask):
+def extract_features(mask, clsrange=[1, 6]):
     counts = []
-    for i in range(1,6):
+    for i in range(clsrange[0], clsrange[1]):
         counts.append(np.count_nonzero(mask == i))
     percents = np.array(counts).astype(np.float32)
     percents /= percents.sum()
