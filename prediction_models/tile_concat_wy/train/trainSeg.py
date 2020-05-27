11d5101af4665c5563b376219e906067c06377fd
wenyuan <liwenyuan.zju@gmail.com>
2020-05-26 22:16:59 -0700

    commit multi-task learning

--- prediction_models/tile_concat_wy/train/trainSeg.py
+++ prediction_models/tile_concat_wy/train/trainSeg.py
@@ -116,7 +116,7 @@ if __name__ == "__main__":
     weightsDir = './weights/{}'.format(fname)
     check_folder_exists(weightsDir)
     # for fold in trange(nfolds, desc='fold'):
-    for fold in range(0,2):
+    for fold in range(2,4):
         trainloader, valloader = crossValData(fold)
         model = Model(arch='deeplabv3_resnet50', n=num_classes).cuda()
         # optimizer = optim.Adam(model.parameters(), lr=1e-3, weight_decay=0)
