2ae78c0bc076b36d47b2b52d68abf8ed0b8fbd25
wenyuan <liwenyuan.zju@gmail.com>
2020-05-27 11:57:51 -0700

    commit multi-task learning

--- prediction_models/tile_concat_wy/train/trainSeg.py
+++ prediction_models/tile_concat_wy/train/trainSeg.py
@@ -29,8 +29,8 @@ class Train(object):
         train_loss = []
         with torch.set_grad_enabled(True):
             for i, data in enumerate(tqdm(trainloader, desc='trainIter'), start=0):
-                if i >= 5:
-                    break
+                # if i >= 5:
+                #     break
                 # get the inputs; data is a list of [inputs, labels]
                 inputs, labels, grade = data
                 # zero the parameter gradients
@@ -52,8 +52,8 @@ class Train(object):
         val_loss, val_label, val_preds = [], [], []
         with torch.no_grad():
             for i, data in enumerate(tqdm(valloader, desc='valIter'), start=0):
-                if i > 5:
-                    break
+                # if i > 5:
+                #     break
                 # get the inputs; data is a list of [inputs, labels]
                 inputs, labels, grade = data
                 # zero the parameter gradients
