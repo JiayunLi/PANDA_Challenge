8a3f08fd3f2cb40642fd34fc069ddf8c81efab52
wenyuan <liwenyuan.zju@gmail.com>
2020-05-28 11:25:05 -0700

    commit seg loss coef gradually decrease

--- prediction_models/tile_concat_wy/train/trainSeg.py
+++ prediction_models/tile_concat_wy/train/trainSeg.py
@@ -44,7 +44,7 @@ class Train(object):
                 loss1 = criterion[0](outputs['out'], labels)
                 loss2 = criterion[0](outputs['aux'], labels)
                 loss3 = criterion[1](outputs['isup_grade'].squeeze(dim=1), grade.float().cuda())
-                loss = self.segcoe * (loss1 + 0.4 * loss2) + loss3
+                loss = self.segcoef * (loss1 + 0.4 * loss2) + loss3
                 train_loss.append(loss.item())
                 loss.backward()
                 self.optimizer.step()
@@ -66,7 +66,7 @@ class Train(object):
                 loss1 = criterion[0](outputs['out'], labels)
                 loss2 = criterion[0](outputs['aux'], labels)
                 loss3 = criterion[1](outputs['isup_grade'].squeeze(dim = 1),grade.float().cuda()) # for regression, grade.cuda())
-                loss = 1e-9 * (loss1 + 0.4 * loss2) + loss3
+                loss = self.segcoef * (loss1 + 0.4 * loss2) + loss3
                 val_loss.append(loss.item())
                 val_label.append(grade.cpu())
                 val_preds.append(outputs['isup_grade'].squeeze(dim=1).cpu())
