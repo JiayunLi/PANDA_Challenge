02ae386acc8f5f46895d095dda476a0cb4d80bb7
wenyuan <liwenyuan.zju@gmail.com>
2020-05-31 11:52:36 -0700

    commit dlv3 ft with aux loss fold2

--- prediction_models/tile_concat_wy/train/train.py
+++ prediction_models/tile_concat_wy/train/train.py
@@ -116,7 +116,7 @@ if __name__ == "__main__":
     weightsDir = './weights/{}'.format(fname)
     check_folder_exists(weightsDir)
     # for fold in trange(nfolds - 1, nfolds, desc='fold'):
-    for fold in range(2, 3):
+    for fold in range(3, 4):
         trainloader, valloader = crossValData(fold)
         model = Model(load = load).cuda()
         # if load:
