38ee43168a571548130c57853324fe31228849dc
wenyuan <liwenyuan.zju@gmail.com>
2020-06-02 12:54:32 -0700

    commit tile_extraction highest reso 64*256*256 based on the mask

--- prediction_models/tile_concat_wy/input/tile_woMask_extraction.py
+++ prediction_models/tile_concat_wy/input/tile_woMask_extraction.py
@@ -4,8 +4,8 @@ from input.tile_extraction import write_2_zip_img
 import pandas as pd
 if __name__ == "__main__":
     TRAIN = '../input/prostate-cancer-grade-assessment/train_images/'  ## train image folder
-    OUT_TRAIN = '../input/panda-32x256x256-tiles-data/val.zip'  ## output image folder
-    csv_file = '../input/panda-32x256x256-tiles-data/wo_mask_val.csv'
+    OUT_TRAIN = '../input/panda-36x256x256-tiles-data/val.zip'  ## output image folder
+    csv_file = '../input/panda-36x256x256-tiles-data/wo_mask_val.csv'
     df = pd.read_csv(csv_file)
     names = list(df['image_id'])
     sz = 256  ## image patch size
