4ebfed0297672db96372c4cf48500662901b7628
wenyuan <liwenyuan.zju@gmail.com>
2020-06-02 13:32:35 -0700

    commit tile_extraction highest reso 64*256*256 based on the mask

--- prediction_models/tile_concat_wy/input/inputPipeline_stiching.py
+++ prediction_models/tile_concat_wy/input/inputPipeline_stiching.py
@@ -62,8 +62,13 @@ class PandaPatchDataset(Dataset):
     def __getitem__(self, idx):
         img_id = self.train_csv.loc[idx, 'image_id']
         fname = os.path.join(self.image_dir, img_id+'.tiff')
-        image = skimage.io.MultiImage(fname)[1]
-        imgs, OK = get_tiles(image, self.image_size, self.N)
+
+        fnames = [os.path.join(self.image_dir, self.train_csv.loc[idx, 'image_id'] + '_' + str(i) + '.png')
+                  for i in range(self.N)]
+        imgs = []
+        for idx, fname in enumerate(fnames):
+            img = self.open_image(fname)
+            imgs.append({'img': img, 'idx': idx})
 
         if self.rand: ## random shuffle the order of tiles
             idxes = np.random.choice(list(range(self.N)), self.N, replace=False)
@@ -141,32 +146,6 @@ class PandaPatchDatasetInfer(Dataset):
         # imgs = [torch.tensor(img) for img in imgs]
         imgs = torch.stack(imgs)
         return {'image': imgs, 'name': name}
-
-def get_tiles(img, tile_size, n_tiles, mode=0):
-    result = []
-    h, w, c = img.shape
-    pad_h = (tile_size - h % tile_size) % tile_size + ((tile_size * mode) // 2)
-    pad_w = (tile_size - w % tile_size) % tile_size + ((tile_size * mode) // 2)
-
-    img2 = np.pad(img, [[pad_h // 2, pad_h - pad_h // 2], [pad_w // 2, pad_w - pad_w // 2], [0, 0]],
-                  mode='constant', constant_values=255)
-    img3 = img2.reshape(
-        img2.shape[0] // tile_size,
-        tile_size,
-        img2.shape[1] // tile_size,
-        tile_size,
-        3
-    )
-
-    img3 = img3.transpose(0, 2, 1, 3, 4).reshape(-1, tile_size, tile_size, 3)
-    n_tiles_with_info = (img3.reshape(img3.shape[0], -1).sum(1) < tile_size ** 2 * 3 * 255).sum()
-    if len(img3) < n_tiles:
-        img3 = np.pad(img3, [[0, n_tiles - len(img3)], [0, 0], [0, 0], [0, 0]], mode='constant', constant_values=255)
-    idxs = np.argsort(img3.reshape(img3.shape[0], -1).sum(-1))[:n_tiles]
-    img3 = img3[idxs]
-    for i in range(len(img3)):
-        result.append({'img': img3[i], 'idx': i})
-    return result, n_tiles_with_info >= n_tiles
 
 def data_transform():
     tsfm = albumentations.Compose([
