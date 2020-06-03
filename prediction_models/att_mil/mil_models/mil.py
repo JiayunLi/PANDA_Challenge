326ee1dfdea23a642b42606fdeff8b1aadafe87b
JiayunLi <jiayunli@g.ucla.edu>
2020-06-02 20:47:26 -0700

    implement multi-scale input

--- prediction_models/att_mil/mil_models/mil.py
+++ prediction_models/att_mil/mil_models/mil.py
@@ -36,7 +36,6 @@ def config_encoder(input_size, num_classes, arch, pretrained):
         encoder = torch.hub.load('facebookresearch/semi-supervised-ImageNet1K-models', arch)
         feature_dim = list(encoder.children())[-1].in_features
         encoder.features = nn.Sequential(*list(encoder.children())[:-2])
-        encoder.fc = None
         # encoder.classifier = nn.Sequential(nn.AdaptiveAvgPool2d(output_size=(1, 1)), Flatten(),
         #                              nn.Linear(in_features=feature_dim, out_features=num_classes, bias=True))
         # encoder.classifier = nn.Sequential(AdaptiveConcatPool2d(), Flatten(), nn.Linear(2 * feature_dim, 512),
