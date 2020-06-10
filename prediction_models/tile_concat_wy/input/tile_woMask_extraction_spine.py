a8970ed227393698a918838565cfd898583bd81c
wenyuan <liwenyuan.zju@gmail.com>
2020-06-10 15:18:22 -0700

    commit spine tile extraction

--- prediction_models/tile_concat_wy/input/tile_woMask_extraction_spine.py
+++ prediction_models/tile_concat_wy/input/tile_woMask_extraction_spine.py
@@ -5,14 +5,14 @@ from utiles import utils
 import pandas as pd
 if __name__ == "__main__":
     TRAIN = '../input/prostate-cancer-grade-assessment/train_images/'  ## train image folder
-    OUT_TRAIN = '../input/panda-36x256x256-tiles-data-spine/val.zip'  ## output image folder
+    OUT_TRAIN = '../input/panda-64x256x256-tiles-data-spine/val.zip'  ## output image folder
     MARKER = "../input/prostate-cancer-grade-assessment/marker_images/"
     csv_file = '../input/panda-36x256x256-tiles-data/wo_mask_val.csv'
     df = pd.read_csv(csv_file)
     names = list(df['image_id'])
     pen_marked_images = [name[:-4] for name in os.listdir(MARKER)]
     sz = 256  ## image patch size
-    N = 36  ## how many patches selected from each slide
+    N = 64  ## how many patches selected from each slide
     print(len(names))  ## only images that have masks
     utils.check_folder_exists(os.path.dirname(OUT_TRAIN))
     Source_Folder = TRAIN
