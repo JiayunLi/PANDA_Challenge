b10a17c9df8d60cd982ed25f617c8564d5488972
wenyuan <liwenyuan.zju@gmail.com>
2020-06-17 00:30:32 -0700

    color jitter and brightness

--- prediction_models/tile_concat_wy/input/inputPipeline_stiching.py
+++ prediction_models/tile_concat_wy/input/inputPipeline_stiching.py
@@ -226,7 +226,7 @@ def data_transform():
         albumentations.Transpose(p=0.5),
         albumentations.VerticalFlip(p=0.5),
         albumentations.HorizontalFlip(p=0.5),
-        albumentations.RGBShift(r_shift_limit=5, g_shift_limit=5, b_shift_limit=5)
+        albumentations.RGBShift(r_shift_limit=10, g_shift_limit=10, b_shift_limit=10)
         # albumentations.RandomBrightnessContrast()
     ])
     return tsfm
