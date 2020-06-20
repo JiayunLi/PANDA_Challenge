60ea24c23a2072997c0e65d041ccbf9624af964a
wenyuan <liwenyuan.zju@gmail.com>
2020-06-20 00:46:12 -0700

    color jitter and brightness

--- prediction_models/tile_concat_wy/train/trainStackBin.py
+++ prediction_models/tile_concat_wy/train/trainStackBin.py
@@ -130,11 +130,11 @@ if __name__ == "__main__":
     nfolds = 4
     fname = f'Resnext50_36patch_overlook_cos_opt_stack_{provider}'
     if provider == "rad":
-        csv_file = '../input/cvs_pkl_files/radboud_{}_fold_train.csv'.format(nfolds)
+        csv_file = '../input/csv_pkl_files/radboud_{}_fold_train.csv'.format(nfolds)
     elif provider == 'kar':
-        csv_file = '../input/cvs_pkl_files/karolinska_{}_fold_train.csv'.format(nfolds)
+        csv_file = '../input/csv_pkl_files/karolinska_{}_fold_train.csv'.format(nfolds)
     else:
-        csv_file = '../input/cvs_pkl_files/{}_fold_train.csv'.format(nfolds)
+        csv_file = '../input/csv_pkl_files/{}_fold_train.csv'.format(nfolds)
     image_dir = '../input/panda-36x256x256-tiles-data-opt/train/'
     # image_dir = '../input/panda-36x256x256-tiles-data/train/'
     bs = 6
