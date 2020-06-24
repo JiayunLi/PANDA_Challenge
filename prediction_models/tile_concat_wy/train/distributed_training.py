1c9604c3d4b3c26bfaced72ab8ae0c20b7a6b0d8
wenyuan <liwenyuan.zju@gmail.com>
2020-06-23 23:45:09 -0700

    commit distributed training

--- prediction_models/tile_concat_wy/train/distributed_training.py
+++ prediction_models/tile_concat_wy/train/distributed_training.py
@@ -35,8 +35,8 @@ class Train(object):
         bar = tqdm(trainloader, desc='trainIter')
         result = OrderedDict()
         for i, data in enumerate(bar, start=0):
-            if i >= 5:
-                break
+            # if i >= 5:
+            #     break
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels = data['img'], data['isup_grade']
             # zero the parameter gradients
@@ -71,8 +71,8 @@ class Train(object):
         result = OrderedDict()
         with torch.no_grad():
             for i, data in enumerate(tqdm(valloader, desc='valIter'), start=0):
-                if i > 50:
-                    break
+                # if i > 50:
+                #     break
                 # get the inputs; data is a list of [inputs, labels]
                 inputs, labels, provider = data['img'], data['isup_grade'], data['datacenter']
                 # zero the parameter gradients
