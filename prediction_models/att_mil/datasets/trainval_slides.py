5eb008280f4711a382871584041f73588bdbf652
JiayunLi <jiayunli@g.ucla.edu>
2020-07-15 21:44:34 -0700

    test slides

--- prediction_models/att_mil/datasets/trainval_slides.py
+++ prediction_models/att_mil/datasets/trainval_slides.py
@@ -278,7 +278,7 @@ class BiopsySlidesSelectedOTF(data.Dataset):
                 counter += 1
             else:
                 for cur_lowij in low_ij:
-                    low_i, low_j = cur_lowij[0][0], cur_lowij[0][1]
+                    low_i, low_j = cur_lowij[0], cur_lowij[0]
                     high_tile = self._get_high_tiles(low_i, low_j, rate, cur_slide, high_im_size, cur_im_shape)
                     instances[counter, :, :, :] = high_tile
                     counter += 1
