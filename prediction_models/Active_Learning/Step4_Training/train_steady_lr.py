ff472faac2ac7ef28bdc375bc1672951c7881ee5
wenyuan <liwenyuan.zju@gmail.com>
2020-08-03 00:05:44 -0700

    commit distributed training

--- prediction_models/Active_Learning/Step4_Training/train_steady_lr.py
+++ prediction_models/Active_Learning/Step4_Training/train_steady_lr.py
@@ -34,8 +34,8 @@ class Train(object):
         bar = tqdm(trainloader, desc='trainIter')
         result = OrderedDict()
         for i, data in enumerate(bar, start=0):
-            # if i >= 5:
-            #     break
+            if i >= 25:
+                break
             # get the inputs; data is a list of [inputs, labels]
             inputs, labels, img_idx = data['img'], data['isup_grade'], data['idx']
             # zero the parameter gradients
@@ -60,8 +60,8 @@ class Train(object):
         result = OrderedDict()
         with torch.no_grad():
             for i, data in enumerate(tqdm(valloader, desc='valIter'), start=0):
-                # if i > 20:
-                #     break
+                if i > 20:
+                    break
                 # get the inputs; data is a list of [inputs, labels]
                 inputs, labels, provider, img_idx = data['img'], data['isup_grade'], data['datacenter'], data['idx']
                 # zero the parameter gradients
